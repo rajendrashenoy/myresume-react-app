1f5a8229bf30e1cb6bc63e6ce7f299c78b18a34a
rajendrashenoy <shenoy.rajendra@gmail.com>
2019-01-10 10:04:27 -0500

    Updates

---
+++ precache-manifest.55b20a9a986c91196d83e4b85dc7981d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39a2991c8afaedd2bcb8",
+    "url": "/rajendrashenoy/myresume-react-app/static/js/runtime~main.39a2991c.js"
+  },
+  {
+    "revision": "15e6ed1f49bd1c5a935d",
+    "url": "/rajendrashenoy/myresume-react-app/static/js/main.15e6ed1f.chunk.js"
+  },
+  {
+    "revision": "11d20d220f3059093600",
+    "url": "/rajendrashenoy/myresume-react-app/static/js/1.11d20d22.chunk.js"
+  },
+  {
+    "revision": "15e6ed1f49bd1c5a935d",
+    "url": "/rajendrashenoy/myresume-react-app/static/css/main.06b105ee.chunk.css"
+  },
+  {
+    "revision": "ed182446d4573a265bd818e382793186",
+    "url": "/rajendrashenoy/myresume-react-app/index.html"
+  }
+];
